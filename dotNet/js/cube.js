505770e08424eefa99cf76256b52fc8d34803294
rbhuenemann <rbhuenemann@gmail.com>
2018-02-09 17:26:01 -0600

    interactive pump added

--- dotNet/js/cube.js
+++ dotNet/js/cube.js
@@ -1,85 +1,117 @@
-/**
- * 
- */
-
-window.addEventListener('DOMContentLoaded', function () {
-
-    //get reference of canvas element from HTML
-    var canvas = document.getElementById('renderCanvas');
-    //load Babylon engine
-    var engine = new BABYLON.Engine(canvas, true);
-
-    /******* generate scene ******/
-
-    var createScene = function () {
-
-        // This creates a basic Babylon Scene object (non-mesh)
-        var scene = new BABYLON.Scene(engine);
-
-        new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 400, 200), scene);
-        var cam = new BABYLON.ArcRotateCamera("ArcRotateCamera", 0, 0, 500, new BABYLON.Vector3(0, 3, 0), scene);
-        cam.attachControl(canvas);
-
-        var toad;
 
 
-        BABYLON.SceneLoader.ImportMesh("", "objects/Roary/", "RoaryOnePiece.obj", scene, function (meshes) {
-            var m = meshes[0];
-            m.position.y = 4;
-            toad = m;
+window.addEventListener('DOMContentLoaded', function() {
+
+        //get reference of canvas element from HTML
+    	var canvas = document.getElementById('renderCanvas');
+        //load Babylon engine
+    	var engine = new BABYLON.Engine(canvas, true);
+        
+    
+                
+        
+
+    	/******* generate scene ******/
+    	
+        var createScene = function() {
+    		
+        	// Create the scene space
+            var scene = new BABYLON.Scene(engine);
+
+            // Add a camera to the scene and attach it to the canvas
+            var camera = new BABYLON.ArcRotateCamera("Camera", -0.65, 0.95, 400, BABYLON.Vector3.Zero(), scene);
+            camera.attachControl(canvas, false);
+            
+            camera.lowerBetaLimit = 0.1;
+            camera.upperBetaLimit = (Math.PI / 2) * 0.99;
+            camera.lowerRadiusLimit = 150;
+            
+            scene.clearColor = new BABYLON.Color3(0, 6, 10);
+  
+
+
+            var light = new BABYLON.PointLight("omni", new BABYLON.Vector3(200, 500, -400), scene);
+
+            
+            var ground = BABYLON.Mesh.CreateGround("ground", 1000, 1000, 1, scene, false);
+            var groundMaterial = new BABYLON.StandardMaterial("ground", scene);
+            groundMaterial.specularColor = BABYLON.Color3.Black();
+            ground.material = groundMaterial;
+
+
+            
+            BABYLON.SceneLoader.ImportMesh("", "objects/", "pump.obj", scene, function (pump) {
+                var pumpMotor = pump[0];
+                pumpMotor.isVisible = true;
+                var pumpHub = pump[1];
+                pumpHub.isVisible = true;
+                var pumpBlade1 = pump[2];
+                pumpBlade1.isVisible = true;
+                var pumpBlade2 = pump[3];
+                pumpBlade2.isVisible = true;
+                var pumpBlade3 = pump[4];
+                pumpBlade3.isVisible = true;
+                var pumpBlade4 = pump[5];
+                pumpBlade4.isVisible = true;
+                
+                var pumpCover = pump[6];
+                pumpCover.isVisible = true;
+                
+
+                
+                
+                var pumpScrew1 = pump[7];
+                pumpScrew1.isVisible = true;
+                var pumpScrew2 = pump[8];
+                pumpScrew2.isVisible = true;
+                var pumpScrew3 = pump[9];
+                pumpScrew3.isVisible = true;
+                var pumpScrew4 = pump[10];
+                pumpScrew4.isVisible = true;
+                
+                pumpCover.actionManager = new BABYLON.ActionManager(scene);
+                pumpCover.actionManager
+        .registerAction(
+            new BABYLON.InterpolateValueAction(
+                BABYLON.ActionManager.OnPickTrigger,
+                pumpCover,
+                'visibility',
+                0,
+                1000
+            )
+        )
+        .then(
+            new BABYLON.InterpolateValueAction(
+                BABYLON.ActionManager.OnPickTrigger,
+                pumpCover,
+                'visibility',
+                1.0,
+                1000
+            )
+        );
+                
+            });
+            
+            
+            
+           
+
+            return scene;
+            
+		}
+        
+        
+        /******* End of the create scene function ******/
+        
+        //call scene
+        var scene = createScene();
+        //register render loop on canvas
+        engine.runRenderLoop(function() {
+            scene.render();
         });
-
-
-
-        var sphere = BABYLON.Mesh.CreateSphere("sphere1", 16, 2, scene);
-        sphere.position.y = 1;
-
-        // Respond to w key down
-        scene.actionManager = new BABYLON.ActionManager(scene);
-        scene.actionManager.registerAction(new BABYLON.ExecuteCodeAction(BABYLON.ActionManager.OnKeyDownTrigger, (evt) => {
-            if (evt.sourceEvent.key == "w") {
-                toad.__W_Pressed = true;
-            }
-        }));
-
-        scene.actionManager.registerAction(new BABYLON.ExecuteCodeAction(BABYLON.ActionManager.OnKeyUpTrigger, (evt) => {
-            if (evt.sourceEvent.key == "w") {
-                toad.__W_Pressed = false;
-            }
-        }));
-
-        scene.registerBeforeRender(function () {
-            if (!scene.isReady()) return;
-
-            if (toad) {
-                if (toad.__W_Pressed) toad.translate(BABYLON.Axis.X, -0.1, BABYLON.Space.LOCAL);
-            }
+        //window resize event handler
+        window.addEventListener('resize', function() {
+            engine.resize();
         });
-
-        var ground = BABYLON.Mesh.CreateGround("floor", 500, 500, 100, scene);
-        ground.position.y = -150;
-
-        var materialGround = new BABYLON.StandardMaterial("groundcolor", scene);
-        ground.material = materialGround;
-        materialGround.diffuseTexture = new BABYLON.Texture("objects/RoaryPoly/road.jpg", scene);
-
-        return scene;
-
-    }
-
-    /******* End of the create scene function ******/
-
-    //call scene
-    var scene = createScene();
-    //register render loop on canvas
-    engine.runRenderLoop(function () {
-
-
-        scene.render();
-    });
-    //window resize event handler
-    window.addEventListener('resize', function () {
-        engine.resize();
-    });
-
-});
+        
+   });
