8eba126786757b0a97fe8b45023445d6383471f8
rbhuenemann <rbhuenemann@gmail.com>
2018-02-21 12:17:10 -0600

    iframe added

---
+++ rightpurdyDotNet -working/js/pieTest.js
@@ -0,0 +1,56 @@
+
+window.addEventListener('DOMContentLoaded', function() {
+
+    //get reference of canvas element from HTML
+    var canvas = document.getElementById('renderCanvas');
+    //load Babylon engine
+    var engine = new BABYLON.Engine(canvas, true);
+
+
+    /******* generate scene ******/
+
+    var createScene = function() {
+        
+        // Create the scene space
+        BABYLON.SceneLoader.Load("", "", engine, function(scene){
+            scene.executeWhenReady(function(){
+
+                
+                
+                
+               
+
+                
+                
+                //end of scene functions
+
+                scene.registerBeforeRender(function () {
+                
+                });
+
+                engine.runRenderLoop(function(){
+
+                    scene.render();
+                });
+            });
+        });
+
+
+        return scene;
+
+    }
+
+    //call scene
+    var scene = createScene();
+    
+    
+    // the canvas/window resize event handler
+    window.addEventListener('resize', function(){
+        engine.resize();
+    });
+ 
+   });
+
+
+
+
