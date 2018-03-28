f657d85ab88392436878cc72a0800737da36168e
Ross <rbhuenemann@gmail.com>
2018-03-28 15:41:26 -0500

    playIframe updated

--- playIframe.js
+++ playIframe.js
@@ -1,6 +1,11 @@
-document.getElementById("start_pump").onclick = function() {myFunction()};
+document.getElementById("start_pump").onclick = function() {load_pump()};
 
-function myFunction() {
+function load_pump() {
     document.getElementById("pump").src = "https://rawgit.com/rhuenemann89/rightpurdyDotnet/master/referenced/pump/iframePump.html";
 }
 
+document.getElementById("start_astro").onclick = function() {load_astro()};
+
+function load_astro() {
+    document.getElementById("astro").src = "https://rawgit.com/rhuenemann89/rightpurdyDotnet/master/referenced/astro.html";
+}
