213cfd609e64ae7f0c3cf9de2124a6b6a4a05b95
Ross <rbhuenemann@gmail.com>
2018-03-28 15:01:33 -0500

    playiframe added

---
+++ playIframe.js
@@ -0,0 +1,9 @@
+function load_pump() {
+        var iframePump = document.getElementById("pump");
+        iframePump.src = "https://rawgit.com/rhuenemann89/rightpurdyDotnet/master/pump.html";
+    }
+
+function load_astro() {
+        var iframeAstro = document.getElementById("astro");
+        iframeAstro.src = "https://rawgit.com/rhuenemann89/rightpurdyDotnet/master/astro.html";
+}
