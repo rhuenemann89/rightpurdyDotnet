00b9ef4e9d259154f5f992348f966bc627b54abb
Ross <rbhuenemann@gmail.com>
2018-04-09 11:32:30 -0500

    new RP interactive website

---
+++ referenced/playIframe.js
@@ -0,0 +1,21 @@
+document.getElementById("start_pump").onclick = function() {load_pump()};
+
+function load_pump() {
+    document.getElementById("pump").src = "https://rawgit.com/rhuenemann89/rightpurdyDotnet/master/pump.html";
+    document.getElementById("astro").src = "astro.png";
+    var x = document.getElementById("start_pump");
+    x.style.display = "none";
+    var x = document.getElementById("start_astro");
+    x.style.display = "block";
+}
+
+document.getElementById("start_astro").onclick = function() {load_astro()};
+
+function load_astro() {
+    document.getElementById("astro").src = "https://rawgit.com/rhuenemann89/rightpurdyDotnet/master/referenced/astro.html";
+    document.getElementById("pump").src = "pumpPic.png";
+    var x = document.getElementById("start_astro");
+    x.style.display = "none";
+    var x = document.getElementById("start_pump");
+    x.style.display = "block";
+}
